b6272ee04189598b2f39f508a674c2156be54392
Chaitan Divagi <natiahc@gmail.com>
2020-04-17 18:10:24 +0530

    Add Single Responsiblity Principle code for ref;
    Signed-off-by: Chaitan Divagi <natiahc@gmail.com>

---
+++ SOLID/SRP.cpp
@@ -0,0 +1,29 @@
+// Single Responsibility Principle : A class should only have a single responsibility
+
+#include <string>
+#include <vector>
+
+struct Journal
+{
+    std::string title;
+    std::vector<std::string> entries;
+
+    explicit Journal(const std::string& title)
+        : title{title}
+    {}
+
+    void add(const std::string& entry)
+    {
+        entries.push_back(entry);
+    }
+};
+
+struct PersistenceManager
+{
+    static void save(const Journal& j, const std::string& filename)
+    {
+        std::ofstream ofs(filename);
+        for (auto& s : j.entries)
+            ofs << s << std::endl;
+    }
+};
