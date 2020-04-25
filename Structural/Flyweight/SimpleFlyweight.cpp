b42f83738b1c7de0861b462fbc241772b99c58cd
Chaitan Divagi <natiahc@gmail.com>
2020-04-25 16:41:26 +0530

    FlyWeight using boost;
    Signed-off-by: Chaitan Divagi <natiahc@gmail.com>

--- Structural/Flyweight/SimpleFlyweight.cpp
+++ Structural/Flyweight/SimpleFlyweight.cpp
@@ -1,6 +1,7 @@
 #include <sstream>
 #include <iostream>
 #include <boost\bimap.hpp>
+#include <boost\flyweight.hpp>
 
 typedef uint32_t key;
 
@@ -49,6 +50,25 @@ protected:
 int User::seed = 0;
 boost::bimap<key, std::string> User::names;
 
+struct User2
+{
+	boost::flyweight<std::string> first_name;
+	boost::flyweight<std::string> last_name;
+
+	User2(const std::string& first_name, const std::string& last_name)
+		: first_name{first_name},
+		  last_name{last_name}
+	{
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const User2& obj)
+	{
+		return os
+			<< "first_name: " << obj.first_name
+			<< " last_name: " << obj.last_name;
+	}
+};
+
 int main(int ac, char* av[])
 {
 	User sam_tavares{ "sam", "Tavares" };
@@ -56,6 +76,17 @@ int main(int ac, char* av[])
 
 	std::cout << "Sam: " << sam_tavares << std::endl;
 	std::cout << "John: " << john_tavares << std::endl;
+
+	// using boost flyweight
+
+	User2 zico_cab{ "Zico", "Cab" };
+	User2 dean_cab{ "Dean", "Cab" };
+
+	std::cout << "zico: " << zico_cab << std::endl;
+	std::cout << "dean: " << dean_cab << std::endl;
+
+	std::cout << std::boolalpha << 
+		(&zico_cab.last_name.get() == &dean_cab.last_name.get());
 
 	return 0;
 }
